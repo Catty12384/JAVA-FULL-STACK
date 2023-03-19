ab98bdf970bd3fd8eaf636d3a72020ba5f730776
Sherin Joys Catherina <120629225+Catty12384@users.noreply.github.com>
2023-03-19 11:07:40 +0530

    WEEK 2 ASSIGNMENT P1

---
+++ Bird.java
@@ -0,0 +1,11 @@
+class Bird extends Animal 
+{
+    void fly() 
+    {
+        System.out.print("I am flying");
+    }
+    void sing()
+    {
+        System.out.println("I am singing");
+    }
+}
