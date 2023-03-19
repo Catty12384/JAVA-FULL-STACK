23d24e167a1ca9ddd0df0554946f45c24e7e7491
Sherin Joys Catherina <120629225+Catty12384@users.noreply.github.com>
2023-03-19 11:12:09 +0530

    WEEK 2 ASSIGNMENT P3

---
+++ College.java
@@ -0,0 +1,11 @@
+public class College 
+{
+    public static void main(String args[])
+    {
+        System.out.println("\t NAME\t\tYEAR OF JOINING\t\t     ADDRESS");
+        Details d= new Details();
+        d.print("Robert",1994,"64C- WallsStreat");
+        d.print("Sam",2000,"68D- WallsStreat");
+        d.print("John",1999,"26B- WallsStreat");
+    }    
+}
---
+++ Details.java
@@ -0,0 +1,7 @@
+public class Details 
+{
+    public void print(String name,int yoj,String addr) 
+    {
+        System.out.println("\t"+name+"\t\t     "+yoj+"\t\t"+addr);
+    }    
+}
